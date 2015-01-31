d1f298710a9d5121879d8f0222f9d24d95b54456
William Mak <william.mak@hotmail.ca>
2015-01-31 12:36:19 -0500

    adding data saving

--- camera.py
+++ camera.py
@@ -4,19 +4,20 @@ sys.path.append('/usr/local/lib/python2.7/site-packages')
 
 import cv2
 import numpy as np
+import json
 
 cam = cv2.VideoCapture(1)
 
 cam.set(10, 0)
 
 frames = []
+swing = {"lines" : [], "centers" : []}
 raw_input("press enter to start recording")
-while True:
-#for i in range(300):
+for i in range(300):
     _, img = cam.read()
- #   frames.append(img)
+    frames.append(img)
 
-#for img in frames:
+for img in frames:
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     
     blue_low = np.array([50, 0, 0])
@@ -40,17 +41,9 @@ while True:
         par = np.polyfit(xd, yd, 1, full=True)
         intercept=int(par[0][1])
         slope=int(640*par[0][0] + intercept)
+        swing["lines"].append((intercept, slope))
         cv2.line(img, (0,intercept), (640, slope), (0,0, 255), 3)
-        #res = cv2.drawKeypoints(blue_res, kp, (0, 255, 0))
-#        _, cnts, _ = cv2.findContours(gres, cv2.RETR_LIST,
-#                cv2.CHAIN_APPROX_TC89_KCOS)
-#        max_cnt = None
-#        max_cnt_area = 0
-#        for cnt in cnts:
-#            area = cv2.contourArea(cnt)
-#            if area > max_cnt_area:
-#                max_cnt = cnt
-#        cv2.drawContours(res, [max_cnt], -1, (0, 0, 255, 2))
+
     green_low = np.array([80, 120, 80])
     green_high = np.array([100, 200, 100])
     green_mask = cv2.inRange(img, green_low, green_high)
@@ -71,11 +64,15 @@ while True:
 
         xavg = int(reduce(lambda x, y: x + y, xd) / len(xd))
         yavg = int(reduce(lambda x, y: x + y, yd) / len(yd))
+        swing["centers"].append((xavg, yavg))
         cv2.circle(img, (xavg, yavg), 50, (0,0, 255))
 
     cv2.imshow("frame", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+print json.dumps(swing)
+f = open("swing.json", "w")
+f.write(json.dumps(swing))
 cam.release()
 cv2.destroyAllWindows()
