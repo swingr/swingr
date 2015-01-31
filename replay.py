3f2d16a5956e676302df17cc4e76ad1c5558677f
William Mak <william.mak@hotmail.ca>
2015-01-31 18:58:24 -0500

    bambam

--- replay.py
+++ replay.py
@@ -2,6 +2,7 @@
 import sys
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm 
+from math import sqrt
 import numpy as np
 import sklearn
 import json
@@ -17,15 +18,12 @@ swing = json.loads(s.read())
 scenter_x = [line[0] for line in swing["centers"]]
 scenter_y = [line[1] for line in swing["centers"]]
 
-
-#for i in range(len(swing["centers"])):
-#    if swing["lines"][i][0]:
-#        cv2.line(img, (0, swing["lines"][i][0]), (640, swing["lines"][i][1]), (0, 255, 0), 3)
-#    if swing["centers"][i][0]:
-#        cv2.circle(img, (swing["centers"][i][0], swing["centers"][i][1]), 50, (0,0, 255))
+dists = []
 for i in range(min(len(scenter_y), len(mcenter_y))):
-    if scenter_x[i] and scenter_y[i]:
-        if mcenter_x[i] and mcenter_y[i]:
-            plt.scatter(scenter_x[i], 500-scenter_y[i], [i,i,i], 0.75)
-            plt.scatter(mcenter_x[i], 500-mcenter_y[i], [i,i,i])
+    if mcenter_x[i] and mcenter_y[i]:
+        if scenter_x[i] and scenter_y[i]:
+            dists.append(sqrt((scenter_y[i] - mcenter_y[i]) ** 2 + (scenter_y[i]
+                - mcenter_y[i]) ** 2))
+print 1 - (np.mean(dists)/max(dists))
+
 plt.show()
